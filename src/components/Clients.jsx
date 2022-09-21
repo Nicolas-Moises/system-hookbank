d8c0cad374580ee16f3ad51dc4d9e37a429865b9
Nicolas Moises <nicolas.nicc21@gmail.com>
2022-09-21 00:32:36 -0300

    first commit

---
+++ src/components/Clients.jsx
@@ -0,0 +1,16 @@
+import { clients } from "../constants";
+import styles from "../style";
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
+          <img src={client.logo} alt="client_logo" className="sm:w-[170px] w-[100px] object-contain opacity-50 hover:opacity-100" />
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default Clients;
